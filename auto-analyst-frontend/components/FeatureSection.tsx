b8ae4352c434772a6cb8335cb5e03c1e29b59c1f
Ashad Qureshi <ashad001sp@gmail.com>
2025-01-31 20:27:03 +0500

    deploy v2

--- auto-analyst-frontend/components/FeatureSection.tsx
+++ auto-analyst-frontend/components/FeatureSection.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { motion } from "framer-motion"
-import { features } from "@/components/Constants";
+import { features } from "./FnT";
 
 export default function FeaturesSection() {
   return (
--- auto-analyst-frontend/components/TestimoniaslSections.tsx
+++ auto-analyst-frontend/components/TestimoniaslSections.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { motion } from "framer-motion"
-import { testimonials } from "@/components/Constants"
+import { testimonials } from "./FnT"
 
 
 export default function TestimonialsSection() {
