7d196564519c52b8ea1b65910f8e516572551131
Ashad Qureshi <ashad001sp@gmail.com>
2025-03-11 17:38:31 +0500

    Reched model costs

--- auto-analyst-backend/scripts/tier_maker.py
+++ auto-analyst-backend/scripts/tier_maker.py
@@ -1,11 +1,11 @@
 # Cost per 1K tokens for different models
 costs = {
     "openai": {
-        "gpt-3.5-turbo": {"input": 0.0005, "output": 0.0015},  
-        "gpt-4.5-preview": {"input": 0.075, "output": 0.15},
         "gpt-4": {"input": 0.03, "output": 0.06},  
         "gpt-4o": {"input": 0.0025, "output": 0.01},  
+        "gpt-4.5-preview": {"input": 0.075, "output": 0.15},
         "gpt-4o-mini": {"input": 0.00015, "output": 0.0006},  
+        "gpt-3.5-turbo": {"input": 0.0005, "output": 0.0015},  
         "o1": {"input": 0.015, "output": 0.06},  
         "o1-mini": {"input": 0.00011, "output": 0.00044},  
         "o3-mini": {"input": 0.00011, "output": 0.00044}  
