48b2bedeedec5f85af6a17f17f284815fe2e3b40
Ashad Qureshi <ashad001sp@gmail.com>
2025-03-19 20:15:59 +0500

    User account UI enhanced

--- auto-analyst-frontend/components/ui/progress.tsx
+++ auto-analyst-frontend/components/ui/progress.tsx
@@ -12,7 +12,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-gray-100",
+      "relative h-3 w-full overflow-hidden rounded-full bg-[#F3F4F6]",
       className
     )}
     {...props}
